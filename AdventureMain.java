59deda221357ec635536a6c11125766534da1f04
Nathan Lakritz <natejl123@gmail.com>
2018-01-20 20:21:16 -0800

    Added welcome message.

---
+++ AdventureMain.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Adventure {
+	static Scanner userInput = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.print("WELCOME, PRESS ENTER TO BEGIN.");
+		userInput.nextLine();
+	}
+}
