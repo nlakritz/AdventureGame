4d14264740bfcec153bc6f187d3054739b7e00c9
Nathan Lakritz <natejl123@gmail.com>
2018-01-22 22:51:47 -0800

    Condense Monster class.

--- Monster.java
+++ Monster.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.stream.IntStream;
+import java.util.*;
 
 public class Monster {
 	private static ArrayList<Integer> arr = new ArrayList<Integer>();
@@ -18,48 +16,40 @@ public class Monster {
 		}
 		return alive;
 	}
-	
+
 	public int getHealth() {
 		return health;
 	}
-	
+
 	public void setHealth(int newhealth) {
 		health = newhealth;
 	}
-	
+
 	public String bodyPart() {
-		int num = (int) (Math.random() * 8); 
+		int num = (int) (Math.random() * 8);
 		String part = "";
-		while (arr.contains(num)) {  // not working for some reason. Trying to get all the num so i dont repeat
-			num = (int) (Math.random() * 8); 
+		while (arr.contains(num)) {
+			num = (int) (Math.random() * 8);
 		}
 		arr.add(num);
-		
-		if(num == 0) {
+
+		if (num == 0) {
 			part = "left leg";
-			return part;
-		} else if(num == 1) {
-			part= "right leg";
-			return part;
-		} else if(num == 2) {
+		} else if (num == 1) {
+			part = "right leg";
+		} else if (num == 2) {
 			part = "left arm";
-			return part;
-		} else if(num == 3) {
+		} else if (num == 3) {
 			part = "right arm";
-			return part;
-		} else if(num == 4) {
+		} else if (num == 4) {
 			part = "tail";
-			return part;
-		} else if(num == 5) {
+		} else if (num == 5) {
 			part = "left head";
-			return part;
-		} else if(num == 6) {
+		} else if (num == 6) {
 			part = "middle head";
-			return part;
-		} else if(num == 7) {
+		} else if (num == 7) {
 			part = "right head";
-			return part;
 		}
-		return "";
+		return part;
 	}
 }
